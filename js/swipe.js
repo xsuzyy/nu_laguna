56f5b8ed4b60b1910c0bcfe77d05581774062591
xsuzyy <86343090+xsuzyy@users.noreply.github.com>
2022-09-27 17:06:47 +0800

    update

--- js/swipe.js
+++ js/swipe.js
@@ -6,7 +6,7 @@ var swiper = new Swiper(".slide-content", {
     fade: 'true',
     grabCursor: 'true',
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-pagination-bullet",
       clickable: true,
       dynamicBullets: true,
     },
@@ -24,6 +24,67 @@ var swiper = new Swiper(".slide-content", {
         },
         950: {
             slidesPerView: 1,
+        },
+    },
+  });
+
+
+  var swiper2 = new Swiper(".slide-content-2", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination-2",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-2",
+      prevEl: ".swiper-button-prev-2",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 1,
+        },
+        950: {
+            slidesPerView: 1,
+        },
+    },
+  });
+
+  var swiper3 = new Swiper(".slide-content-3", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination-3",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-3",
+      prevEl: ".swiper-button-prev-3",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
         },
     },
   });
